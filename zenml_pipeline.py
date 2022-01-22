2fb74a0b4615f39ff84d5b016a84f9f0c9085b23
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-01-22 01:06:04 +0100

    Hamya Usage only - Failed state

--- zenml_pipeline.py
+++ zenml_pipeline.py
@@ -1,41 +1,44 @@
-from datasource import get_dataset
+from NetworkMaterializer import NetworkMaterializer
+from datasource import get_trainset, get_testset
 import torch
 from network import Net
-from trainer import CIFAR_trainer
-from pytorch_image_classifier import save_classifier
+from trainer import cifar_trainer
+from torch.utils.data import DataLoader
 
 from zenml.pipelines import pipeline
 from zenml.steps import step, Output
 
 
-@step
-def importer() -> (torch.utils.data.DataLoader, torch.utils.data.DataLoader):
+# @step
+def importer() -> (DataLoader, DataLoader):
     """
     Download the CIFAR data.
     """
-    train, test = get_dataset()
+    train = get_trainset()
+    test = get_testset()
     return train, test
 
 
 @step
-def trainer(
-    train_loader: torch.utils.data.DataLoader
-) -> Net:
+def trainer() -> Output(network=Net):
     """A simple pytorch Model to train on the data."""
-    net = CIFAR_trainer(train_loader)
-    return net
+    # train, test = importer()
+    train = get_trainset()
+    network = cifar_trainer(train)
+    return network
 
 
 @step
 def evaluator(
-    net: Net,
-    test_loader: torch.utils.data.DataLoader
+    net: Net
 ) -> float:
     """Calculate the accuracy on the test set"""
     correct = 0
     total = 0
+    test_loader = get_testset()
+    print(test_loader)
     with torch.no_grad():
-        for data in test_loader:
+        for _, data in enumerate(test_loader, 0):
             images, labels = data
             outputs = net(images)
             _, predicted = torch.max(outputs.data, 1)
@@ -57,29 +60,30 @@ def save_model(net: Net) -> None:
     :param net: The model that is learned during the training
     :return: Nothing
     """
-    save_classifier(net)
+    # save_classifier(net)
 
 
 @pipeline
 def cifar_pipeline(
-    importer,
-    trainer,
-    evaluator,
-    save_model,
+    # _importer,
+    _trainer,
+    _evaluator,
+    # _save_model,
 ):
     """Links all the steps together in a pipeline"""
-    train, test = importer()
-    model = trainer(train)
-    evaluator(model, test)
-    save_model(model)
+
+    network = _trainer()
+    _evaluator(network)
+    # _save_model(net)
 
 
 if __name__ == "__main__":
     # Run the pipeline
     p = cifar_pipeline(
-        importer=importer(),
-        trainer=trainer(),
-        evaluator=evaluator(),
-        save_model=save_model()
+        # _importer=importer(),
+        _trainer=trainer().with_return_materializers(NetworkMaterializer),
+        _evaluator=evaluator()
+        # _evaluator=evaluator().with_return_materializers(DataLoaderMaterializer),
+        # _save_model=save_model()
     )
     p.run()
